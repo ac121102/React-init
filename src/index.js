3b54c87011955da5c4380c6104072632077db639
Arvind <ac121102@gmail.com>
2023-03-04 17:00:26 +0530

    new file: index.css

--- src/index.js
+++ src/index.js
@@ -1,17 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import reactDom from "react-dom";
+import './index.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function BookList() {
+  return (
+    <section className="booklist">
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+/* Book Component */
+const Book = () => {
+  return (
+    <article className="book">
+      <Image></Image>
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+/* Image Component */
+const Image = () => {
+  return (
+    <img
+      src="https://images-eu.ssl-images-amazon.com/images/I/41ka4efHjBL._AC_SX368_.jpg"
+      alt=""
+    />
+  );
+};
+
+/* Title Component */
+const Title = () => {
+  return <h1>8 Rules of Love : How to Find it, Keep it, and Let it Go</h1>;
+};
+
+/* Author Component */
+const Author = () => {
+  return <h4 style={}>Jay Shetty</h4>;
+};
+
+reactDom.render(<BookList />, document.getElementById("root"));
