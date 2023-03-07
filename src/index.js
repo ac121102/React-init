78c01a4f22bd0860ac6f51da39a33e2a48a0dfb6
Arvind <ac121102@gmail.com>
2023-03-07 22:44:17 +0530

    Added props

--- src/index.js
+++ src/index.js
@@ -1,48 +1,63 @@
-import React from "react";
-import reactDom from "react-dom";
-import './index.css'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+
+/* Setup Global Variables */
+
+const firstBook = {
+  img: 'https://images-eu.ssl-images-amazon.com/images/I/41cqBP3CKoL._AC_SX368_.jpg',
+  title: 'The Indian Metropolis',
+  author: 'Feroze Varun Gandhi',
+};
+
+const secondBook = {
+  img: 'https://m.media-amazon.com/images/I/51-nXsSRfZL._SY344_BO1,204,203,200_.jpg',
+  title: 'Atomic Habits',
+  author: 'James Clear',
+};
+
+const thirdBook = {
+  img: 'https://m.media-amazon.com/images/I/814L+vq01mL._AC_UY436_FMwebp_QL65_.jpg',
+  title: 'Ikigai',
+  author: 'Hector Garcia',
+};
 
 function BookList() {
   return (
-    <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+    <section className='booklist'>
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      />
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
+      <Book
+        img={thirdBook.img}
+        title={thirdBook.title}
+        author={thirdBook.author}
+      />
     </section>
   );
 }
 
 /* Book Component */
-const Book = () => {
+
+const Book = (props) => {
   return (
-    <article className="book">
-      <Image></Image>
-      <Title />
-      <Author />
+    <article className='book'>
+      <img
+        src={props.img}
+        alt=''
+      />
+      <h1>{props.title}</h1>
+      <h4>{props.author}</h4>
     </article>
   );
 };
 
-/* Image Component */
-const Image = () => {
-  return (
-    <img
-      src="https://images-eu.ssl-images-amazon.com/images/I/41ka4efHjBL._AC_SX368_.jpg"
-      alt=""
-    />
-  );
-};
-
-/* Title Component */
-const Title = () => {
-  return <h1>8 Rules of Love : How to Find it, Keep it, and Let it Go</h1>;
-};
-
-/* Author Component */
-const Author = () => {
-  return <h4 style={{}}>Jay Shetty</h4>;
-};
-
-reactDom.render(<BookList />, document.getElementById("root"));
+const root = createRoot(document.getElementById('root'));
+root.render(<BookList />);
