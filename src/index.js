0ff6227b56cc72780befeb93a8645c477c53e5f0
Arvind <ac121102@gmail.com>
2023-03-07 23:47:58 +0530

    Added List iteration

--- src/index.js
+++ src/index.js
@@ -2,44 +2,38 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 
-/* Setup Global Variables */
+/* Global Variables */
 
-const firstBook = {
-  img: 'https://images-eu.ssl-images-amazon.com/images/I/41cqBP3CKoL._AC_SX368_.jpg',
-  title: 'The Indian Metropolis',
-  author: 'Feroze Varun Gandhi',
-};
+const books = [
+  {
+    id: 1,
+    img: 'https://images-eu.ssl-images-amazon.com/images/I/41cqBP3CKoL._AC_SX368_.jpg',
+    title: 'The Indian Metropolis',
+    author: 'Feroze Varun Gandhi',
+  },
+  {
+    id: 2,
+    img: 'https://m.media-amazon.com/images/I/51-nXsSRfZL._SY344_BO1,204,203,200_.jpg',
+    title: 'Atomic Habits',
+    author: 'James Clear',
+  },
+  {
+    id: 3,
+    img: 'https://m.media-amazon.com/images/I/814L+vq01mL._AC_UY436_FMwebp_QL65_.jpg',
+    title: 'Ikigai',
+    author: 'Hector Garcia',
+  },
+];
 
-const secondBook = {
-  img: 'https://m.media-amazon.com/images/I/51-nXsSRfZL._SY344_BO1,204,203,200_.jpg',
-  title: 'Atomic Habits',
-  author: 'James Clear',
-};
-
-const thirdBook = {
-  img: 'https://m.media-amazon.com/images/I/814L+vq01mL._AC_UY436_FMwebp_QL65_.jpg',
-  title: 'Ikigai',
-  author: 'Hector Garcia',
-};
+/* BookList Component */
 
 function BookList() {
   return (
     <section className='booklist'>
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      />
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      />
-      <Book
-        img={thirdBook.img}
-        title={thirdBook.title}
-        author={thirdBook.author}
-      />
+      {books.map((book) => {
+        // return (<Book key={book.id} {...book}></Book>);
+        return Book(book);
+      })}
     </section>
   );
 }
@@ -47,17 +41,20 @@ function BookList() {
 /* Book Component */
 
 const Book = (props) => {
+  const { img, title, author } = props;
   return (
     <article className='book'>
       <img
-        src={props.img}
+        src={img}
         alt=''
       />
-      <h1>{props.title}</h1>
-      <h4>{props.author}</h4>
+      <h1>{title}</h1>
+      <h4>{author}</h4>
     </article>
   );
 };
 
-const root = createRoot(document.getElementById('root'));
+const root = createRoot(
+  document.getElementById('root')
+);
 root.render(<BookList />);
