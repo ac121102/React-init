9f8d6dff7cadad1327af842dae8114e995debcde
Arvind <ac121102@gmail.com>
2023-03-27 04:12:38 +0530

    added import export

---
+++ src/books.js
@@ -0,0 +1,20 @@
+export const books = [
+  {
+    id: 1,
+    img: 'https://images-eu.ssl-images-amazon.com/images/I/41cqBP3CKoL._AC_SX368_.jpg',
+    title: 'The Indian Metropolis',
+    author: 'Feroze Varun Gandhi',
+  },
+  {
+    id: 2,
+    img: 'https://m.media-amazon.com/images/I/51-nXsSRfZL._SY344_BO1,204,203,200_.jpg',
+    title: 'Atomic Habits',
+    author: 'James Clear',
+  },
+  {
+    id: 3,
+    img: 'https://m.media-amazon.com/images/I/814L+vq01mL._AC_UY436_FMwebp_QL65_.jpg',
+    title: 'Ikigai',
+    author: 'Hector Garcia',
+  },
+];
--- src/index.js
+++ src/index.js
@@ -1,29 +1,9 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
+import { books } from './books';
 
 /* Global Variables */
-
-const books = [
-  {
-    id: 1,
-    img: 'https://images-eu.ssl-images-amazon.com/images/I/41cqBP3CKoL._AC_SX368_.jpg',
-    title: 'The Indian Metropolis',
-    author: 'Feroze Varun Gandhi',
-  },
-  {
-    id: 2,
-    img: 'https://m.media-amazon.com/images/I/51-nXsSRfZL._SY344_BO1,204,203,200_.jpg',
-    title: 'Atomic Habits',
-    author: 'James Clear',
-  },
-  {
-    id: 3,
-    img: 'https://m.media-amazon.com/images/I/814L+vq01mL._AC_UY436_FMwebp_QL65_.jpg',
-    title: 'Ikigai',
-    author: 'Hector Garcia',
-  },
-];
 
 /* BookList Component */
 
@@ -31,8 +11,12 @@ function BookList() {
   return (
     <section className='booklist'>
       {books.map((book) => {
-        // return (<Book key={book.id} {...book}></Book>);
-        return Book(book);
+        return (
+          <Book
+            key={book.id}
+            {...book}
+          ></Book>
+        );
       })}
     </section>
   );
@@ -42,6 +26,11 @@ function BookList() {
 
 const Book = (props) => {
   const { img, title, author } = props;
+
+  const clickHandler = () => {
+    alert('Hello');
+  };
+
   return (
     <article className='book'>
       <img
@@ -50,6 +39,10 @@ const Book = (props) => {
       />
       <h1>{title}</h1>
       <h4>{author}</h4>
+      <button
+        type='button'
+        onClick={clickHandler}
+      ></button>
     </article>
   );
 };
